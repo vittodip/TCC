14040daf5b4ce67bd01653fb63b26204e98c213e
Victor de Paula Freitas Simões <simoesvictor188@gmail.com>
2022-09-19 17:39:42 -0300

    ok

--- site/src/routes.js
+++ site/src/routes.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lading from './pages/lading-page/index.js';
 import CadastroVoluntario from './pages/cadastro/cadastroVoluntario/index.js';
 
-import PerfilUsuario from './pages/perfil-usuario/index.js';
-import CadastroPaciente from './pages/cadastro/cadastroPaciente/index.js'
+import PerfilUsuario from './pages/perfil/perfilUsuario/index.js'
+import CadastroPaciente from './pages/perfil/perfilVoluntario/index.js'
 
 
 
