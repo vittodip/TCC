4140f166ae0d178898947244537968231bb0673b
Victor de Paula Freitas Simões <simoesvictor188@gmail.com>
2022-09-21 15:21:43 -0300

    carregar solicitação interminado

--- api/src/repository/solicitacaoRepository.js
+++ api/src/repository/solicitacaoRepository.js
@@ -2,10 +2,10 @@ import { con } from './connection.js';
 
 
 export async function fazerSolicitacao(solicitacao) {
-    const comando = `insert into tb_solicitacao (id_usuario, id_psicologo, ds_solicitacao, ds_situacao, dt_situacao)
+    const comando = `insert into tb_solicitacao (id_usuario, ds_solicitacao, ds_situacao, dt_situacao)
     values (?, ?, ?, false, now())`
 
-    const [resp] = await con.query(comando, [solicitacao.idUsuario, solicitacao.idVoluntario, solicitacao.assunto, solicitacao.data ])
+    const [resp] = await con.query(comando, [solicitacao.idUsuario, solicitacao.assunto, solicitacao.data ])
     solicitacao.idSolicitacao = resp.insertId
     return solicitacao;
 }
@@ -14,8 +14,8 @@ export async function mostrarSolicitacao(id){
     const comando = `select 
            id_solicitacao solicitacao,
            id_usuario 	  usuario,
-           id_psicologo   psicologo,
            ds_solicitacao texto,
+           ds_situacao    situacao,
            dt_situacao 	  horario
           
     from tb_solicitacao
