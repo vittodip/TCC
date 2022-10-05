41347196192575fcd50e32ac10a8ad3fd08992c9
Eduardo Cesar Silva de Ávila <unkeniy8432@gmail.com>
2022-10-05 15:01:29 -0300

    Politica de privacidade concluída.

---
+++ site/src/components/logos/logobranca.js
@@ -0,0 +1,10 @@
+
+
+export default function LogoHorizontalBranca(){
+
+    return(
+        <div>
+            <img src="/assets/images/logohorizontalbranca.svg" alt="" />
+        </div>
+    )
+}
