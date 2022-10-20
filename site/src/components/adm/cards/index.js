7a29ceea9380320b48fc54420187ee948ea09f55
Bruno <bruno.virgilio06@gmail.com>
2022-10-19 23:06:30 -0300

    Tentativa falha de arrumar a imagem

--- site/src/components/adm/cards/index.js
+++ site/src/components/adm/cards/index.js
@@ -1,53 +1,57 @@
 import "./index.scss";
 
-
 export default function CardsAdmin(props) {
-
-    
-
-    
-
- 
-
-
-
   return (
-    <div>
-        {props.tipo === "A ser aprovado" && (
-           
-            //"section-cad-container"
-          <div className={props.css}>
+
+    <div className="card">
+      {props.tipo === "A ser aprovado" && (
+        //"section-cad-container"
+
+        <div className={props.css}>
+          <div className="container-colunas-alinhamento">
             <div className="container-colunas">
-              <div className="colunas-alinhamento">
+              <div>
                 <label>Nome</label>
                 <p>{props.nome}</p>
               </div>
-              <div className="colunas-alinhamento">
+              <div>
                 <label>Telefone</label>
                 <p>{props.telefone}</p>
               </div>
-              <div className="colunas-alinhamento">
+              <div>
                 <label>CPF</label>
                 <p>{props.cpf}</p>
               </div>
             </div>
             <div className="container-colunas">
-              <div className="colunas-alinhamento">
+              <div>
                 <label>E-mail</label>
                 <p>{props.email}</p>
               </div>
-              <div className="colunas-alinhamento">
+              <div>
                 <label>Data de Nascimento</label>
                 <p>{props.data}</p>
               </div>
-              <div className="colunas-alinhamento">
+              <div>
+
                 <label>CRP</label>
                 <p>{props.crp}</p>
               </div>
             </div>
           </div>
-          )}
-          
-          </div>
+          <div className="botoes">
+              <button>
+                <img src="/assets/images/botao-aprovar.svg" />
+              </button>
+              <button>
+                <img src="/assets/images/botao-reprovar.svg" />
+              </button>
+            </div>
+        </div>
+      )}
+
+      
+    </div>
+
   );
 }
