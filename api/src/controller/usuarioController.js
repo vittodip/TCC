b4c32c01f1e4da5b5459d71dac15cbc156954831
jennifxz <jenniferoliveira362@gmail.com>
2022-10-30 01:44:17 -0300

    html email

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -200,9 +200,39 @@ server.post('/enviar-email', async (req, resp) =>{
      to: data.email,
      subject:'Nat',
      html: `
-     <div>
-        <h2>Recuperar Senha</h2>
-    </div>
+     <div style="background-color:#fbdcff;padding:10px;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+    <div style="max-width:650px;margin:0 auto">
+        <div style="background:#fff;font-size:14pt;color:#686f7a;border-top:4px solid rgb(120, 0, 175);margin-bottom:20px">
+        
+            <div style="padding:10px 20px;clear:both">
+                <div style="font-size:0.8em;line-height:1.5em;border-bottom:1px solid #f2f3f5;padding-bottom:10px;margin-bottom:10px;">
+                    <p>
+                        <a style="text-decoration:none;color:black">
+                            <b>Redefinição de Senha</b>
+                        </a>
+                    </p>
+                    <div style="color:#000">
+                        <p style="color:rgb(113, 50, 142)">
+                            Oi</b>! <br>
+                            <b style="color:#000000;">Para redefinir sua senha, basta seguir o link abaixo:</b> <br>
+                            
+                        </p>
+                        <a href="http://localhost:3000/login/esqueci-senha">
+                            <button style="color: white; background-color:rgb(113, 50, 142); border: none; border-radius: 20px; padding: 1em; outline: none;"  >Clique aqui!</button>
+                        </a>
+                        <p style="color:#000">
+                            Até logo! =) 
+                        </p>
+                    </div>
+                    <p style="font-size:14px;margin-top:50px;">
+                        Atenciosamente,<br>
+                        Equipe do Need A Talk. <br>
+                        <span style="color:#A52A2A"> ** Este é um e-mail automático, não responda. </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
      `
      
     }
