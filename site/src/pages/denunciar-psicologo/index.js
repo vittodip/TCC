43772330444b1862a21899b27ce123763eb60890
jennifxz <jenniferoliveira362@gmail.com>
2022-10-30 02:53:47 -0300

    termino amaña

--- site/src/pages/denunciar-psicologo/index.js
+++ site/src/pages/denunciar-psicologo/index.js
@@ -1,48 +1,48 @@
-import './index.scss' 
+import './index.scss'
 
 export default function DenunciarPsicologo() {
-    return(
+    return (
         <main className='denunciar-psicologo-main'>
-            
+
             <div className='painel-linear'>
                 <div className='header-painel'>
-                    <img src='/assets/images/logonat.png'/>
+                    <img src='/assets/images/logonat.png' />
 
                     <button>Perfil</button>
                 </div>
-                <div>
-                <div>
-                <h2>Denunciar Psicólogo</h2>
-                <p>Sua denuncia será enviada e revisada por administradores.
-                    Ela é muito importante para a manutenção da segurança de nosso site!
-                </p>
+                <div className='alinhamento-linear'>
+                    <div className='titulo-desc'>
+                        <h2>Denunciar Psicólogo</h2>
+                        <p>Sua denuncia será enviada e revisada por administradores.
+                            Ela é muito importante para a manutenção da segurança de nosso site!
+                        </p>
+                    </div>
+                    <img className='manutencao-img' src='/assets/images/manutencao.png' />
                 </div>
-                </div>  
-                
-                <img className='manutencao-img' src='/assets/images/manutencao.png'/>
-            </div>  
-            <div>
-                <div>
-                    <label>
-                        Psicólogo
+
+            </div>
+            <div className='campos-denuncia-psic'>
+                <div className='colunas'>
+                    <div>
+                        <label>Psicólogo</label>
                         <input placeholder='Nome e sobrenome' />
-                    </label>
-                    <label>
-                        E-mail Psicólogo
+                        <label>
+                            E-mail Psicólogo
+                        </label>
                         <input placeholder='example@example.com' />
-                    </label>
-                </div>
-                <div>
-                <label>
-                        Paciente
+                    </div>
+                    <div>
+                        <label>
+                            Paciente
+                        </label>
                         <input placeholder='Nome e sobrenome' />
-                    </label>
-                    <label>
-                        E-mail Paciente
+                        <label>
+                            E-mail Paciente
+                        </label>
                         <input placeholder='example@example.com' />
-                    </label>
-                </div>
-                <div>
+                    </div>
+                    <div>
+                    </div>
                     <textarea></textarea>
                     <button>Enviar</button>
                 </div>
